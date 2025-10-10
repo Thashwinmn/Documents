938ab0f1c57d2486e6d18d77ea82a845d01f754f
Thashwinmn <thashwinmn525@gmail.com>
2025-10-10 20:33:54 +0530

    My python program

---
+++ first program.py
@@ -0,0 +1,11 @@
+print("Hello world! This is my first Python program")
+name="Thashwin.m.n"
+age=18
+print("My Name is :",name)
+print("My Age is :",age)
+a=2
+b=4
+sum=a+b
+print(sum)
+age2=age
+print(age2)
